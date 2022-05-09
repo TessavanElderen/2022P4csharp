4cf2fc20e1aafb6144969efcdd8bc94c0c643510
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2022-05-09 12:07:42 +0200

    list brons en silver afgemaakt

--- 2022P4csharp/2022P4csharp/Program.cs
+++ 2022P4csharp/2022P4csharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace _2022P4csharp
 {
@@ -7,6 +9,31 @@ namespace _2022P4csharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            List<string> birds = new List<string>() { "duif", "roodbosje" };
+            birds.Add("kraai");
+            birds.Add("papegaai");
+            birds.Add("uil");
+            birds.Remove("hond");
+
+            List<string> birds2 = new List<string>();
+            birds2.Add("meeuw");
+            birds2.Add("arend");
+
+            foreach (string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+            Console.ReadLine();
         }
+
+
     }
+
+    internal class Enemy
+    {
+
+    }
+
+
 }
