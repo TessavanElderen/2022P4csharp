1d30a9ea8901671509ff99d0147588689eb264c0
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2022-05-09 12:46:30 +0200

    Enum afgemaakt

---
+++ 2022P4csharp/Enums/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        enum Colors
+        {
+            red, 
+            blue, 
+            green,
+            yellow
+        }
+    }
+}
