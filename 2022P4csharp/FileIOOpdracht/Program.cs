d160c71b8654eb9da97b9f58f81b721b414e05ff
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-06-15 14:27:18 +0200

    extra opdrachten af

---
+++ 2022P4csharp/FileIOOpdracht/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace FileIOOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] Text = File.ReadAllLines(@"C:\MA\Bewijzenmap\Periode 1.4\C#2\2022P4csharp\2022P4csharp\FileIOOpdracht\Test.txt");
+
+            foreach (string line in Text)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ 2022P4csharp/StringOpdracht/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace StringOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string stringtext = File.ReadAllText(@"C:\MA\Bewijzenmap\Periode 1.4\C#2\2022P4csharp\2022P4csharp\StringOpdracht\stringsplit.txt");
+
+            string[] subs = stringtext.Split(':', ',');
+            foreach (string sub in subs)
+            {
+                Console.WriteLine($"Substring: {sub}");
+            }
+        }
+    }
+}
